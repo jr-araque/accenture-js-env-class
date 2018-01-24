176a581e31d4aee024b1c83a53824ff54819cec6
Jose Reinaldo Araque <jr.araque2@gmail.com>
2018-01-24 16:39:41 -0600

    Adds redirect to new Repo

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.redirect(301, 'https://accn-lessons.herokuapp.com/'))
+
+app.listen( process.env.PORT , (err) => {
+    if (err) console.log(err)
+    else console.log('Application started on... ' + HOST + ":" + process.env.PORT)
+})
